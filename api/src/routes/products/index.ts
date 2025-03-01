81191b31e3325e0327c83cedc428aad4f1be25f0
Pritha Karki <prithakarki030@gmail.com>
2025-03-01 16:48:54 +0545

    Products Router and Controllers

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,16 +1,19 @@
 import { Router } from "express";
+import {
+  listProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "@routes/products/controller";
 
 // products endpoints
 const router = Router();
-router.get("/", (req, res) => {
-  res.send("the list of products");
-});
-router.get("/:id", (req, res) => {
-  res.send("A product");
-});
 
-router.post("/", (req, res) => {
-  res.send("New product created");
-});
+router.get("/", listProducts);
+router.get("/:id", getProductById);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
