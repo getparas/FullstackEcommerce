9dcb42e3d1ecfca7c133bf0f414015eb45a27792
Pritha Karki <prithakarki030@gmail.com>
2025-03-01 17:00:05 +0545

    json parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,12 @@
 import "tsconfig-paths/register";
-import express, { Router } from "express";
+import express, { json, urlencoded } from "express";
 import productsRoutes from "@routes/products";
-const app = express();
+
 const port = 3000;
+const app = express();
+
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
